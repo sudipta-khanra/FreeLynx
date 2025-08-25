a6cb49b071d37d51db7876260d58e6fbbdb91059
sudipta khanra <sudiptakhanra0612@gmail.com>
2025-08-25 09:28:21 +0530

    error correction

--- src/lib/socket.js
+++ src/lib/socket.js
@@ -5,9 +5,10 @@ let socket;
 
 export const connectSocket = (token) => {
   if (!socket) {
-    socket = io("http://localhost:5000", {
+    socket = io("https://freelynx-backend.onrender.com", {
       auth: { token }, // optional if you use token-based auth
     });
+
 
     socket.on("connect", () => {
       console.log("Connected to socket:", socket.id);
