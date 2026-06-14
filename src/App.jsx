bc9363563de3f7c43e35ff8e99810a10a3f48c49
Sudipta Khanra <sudiptakhanra0612@gmail.com>
2026-06-14 20:32:41 +0530

    under maintenance

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
-import React from "react";
-import AppRoutes from "./routes/AppRoutes";
-import { ToastProvider } from "./context/ToastContext";
+// import React from "react";
+// import AppRoutes from "./routes/AppRoutes";
+// import { ToastProvider } from "./context/ToastContext";
+
+// export default function App() {
+//   return (
+//     <ToastProvider>
+//       <AppRoutes />
+//     </ToastProvider>
+
+//   );
+// }
+import React from 'react';
 
 export default function App() {
   return (
-    <ToastProvider>
-      <AppRoutes />
-    </ToastProvider>
-    
+    <div>
+      <h1>The App is under maintenance</h1>
+    </div>
   );
 }
